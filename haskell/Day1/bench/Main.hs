65d8db78c52ae5e5407645aba9a3c8cd41b4af6f
FABIÁN MATÍAS VEGA ALCOTA <fabian.vegaa@sansano.usm.cl>
2022-12-01 23:11:34 -0300

    chore(bench): add bench

--- haskell/Day1/bench/Main.hs
+++ haskell/Day1/bench/Main.hs
@@ -1,10 +1,16 @@
 module Main where
 
 import Criterion.Main
-import Lib (mySum)
+import Lib (maxCaloryElves, maxThreeCaloryElves)
 
 main :: IO ()
-main =
+main =do
+  case0 <- readFile "input/case0.txt"
+  case1 <- readFile "input/case1.txt"
   defaultMain
-    [ bench "Day1" $ whnf mySum [1 .. 99999]
+    [ bench "Day1 - part 1 - case0" $ whnf maxCaloryElves case0,
+      bench "Day1 - part 1 - case1" $ whnf maxCaloryElves case1,
+      bench "Day1 - part 2 - case0" $ whnf maxThreeCaloryElves case0,
+      bench "Day1 - part 2 - case1" $ whnf maxThreeCaloryElves case1
     ]
+
